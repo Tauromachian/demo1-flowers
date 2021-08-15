03cb98058489201a199bd0de07213ea3cdc2d4e0
Jose Garcia <jogarcia@tuta.io>
2021-08-15 15:12:02 -0400

    Add basic sections

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <section-reserve />
+      <section-public-services />
+      <section-recreation-and-culture />
+      <section-beauty />
+      <section-home-services />
     </v-col>
   </v-row>
 </template>
