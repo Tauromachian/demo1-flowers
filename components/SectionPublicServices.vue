180094e7d429244581d25e522918961420d93790
Jose Garcia <jogarcia@tuta.io>
2021-08-15 15:06:49 -0400

    Create basic SectionPublicServices

---
+++ components/SectionPublicServices.vue
@@ -0,0 +1,18 @@
+<template>
+  <carousel-section title="Servicios Públicos" />
+</template>
+
+<script>
+import CarouselSection from './CarouselSection.vue'
+
+export default {
+  name: 'SectionPublicServices',
+  components: {
+    CarouselSection
+  }
+}
+</script>
+
+<style>
+
+</style>
