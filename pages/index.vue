c316e07413fb8aebbd234b63703f8c97ea16b7a1
Jose Garcia <jogarcia@tuta.io>
2021-08-15 14:45:04 -0400

    Add section reserve component

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6" />
+    <v-col cols="12" sm="8" md="6">
+      <section-reserve />
+    </v-col>
   </v-row>
 </template>
