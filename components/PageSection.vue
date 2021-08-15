249d31d315c999d1eab96278cb231cdfd72faca1
Jose Garcia <jogarcia@tuta.io>
2021-08-15 14:41:38 -0400

    Create basic PageSection component

---
+++ components/PageSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-section>
+    <h1 class="page-section__title">
+      {{ title }}
+    </h1>
+  </base-section>
+</template>
+
+<script>
+import BaseSection from './BaseSection.vue'
+
+export default {
+  components: {
+    BaseSection
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.page-section__title {
+  text-transform: uppercase;
+  text-align: center;
+}
+</style>
