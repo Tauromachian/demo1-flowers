7bde58ed9516739744979117ad5305026cbc0949
Jose Garcia <jogarcia@tuta.io>
2021-08-15 14:44:52 -0400

    Create basic section reserve

---
+++ components/SectionReserve.vue
@@ -0,0 +1,23 @@
+<template>
+  <page-section title="Encuentra y reserva el servicio que necesitas">
+    <v-form>
+      <v-text-field label="Qué" />
+      <v-text-field label="Dónde" />
+      <v-btn>Buscar</v-btn>
+    </v-form>
+  </page-section>
+</template>
+
+<script>
+import PageSection from './PageSection.vue'
+
+export default {
+  components: {
+    PageSection
+  }
+}
+</script>
+
+<style>
+
+</style>
