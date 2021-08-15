d9f8723987f66ed09575fc7e3827f4cb9a2cfb11
Jose Garcia <jogarcia@tuta.io>
2021-08-15 15:06:33 -0400

    Create basic CarouseSection

---
+++ components/CarouselSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <content-section :title="title">
+    <v-carousel v-if="slides.length" multiple>
+      <v-carousel-item v-for="(slide, index) in slides" :key="index" />
+    </v-carousel>
+  </content-section>
+</template>
+
+<script>
+import ContentSection from './ContentSection.vue'
+export default {
+  components: {
+    ContentSection
+  },
+  props: {
+    slides: {
+      type: Array,
+      default: () => []
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
