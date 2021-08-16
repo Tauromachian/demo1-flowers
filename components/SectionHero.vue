7f2af83dc61773e43df99c352baa36f4f58c42b4
Jose Garcia <jogarcia@tuta.io>
2021-08-16 15:48:26 -0400

    Create basic hero section

---
+++ components/SectionHero.vue
@@ -0,0 +1,27 @@
+<template>
+  <content-section>
+    <v-row>
+      <v-col cols="6">
+        <h1>Ticket</h1>
+        <p>
+          Es la forma más fácil y rápida de gestionar su agenda de servicios.
+          Convierta su nuevo cliente en un cliente habitual, ofreciendo atención
+          diferenciada y sin hacerlo esperar
+        </p>
+        <v-btn>Resgistre su negocio</v-btn>
+        <v-btn>
+          Y pruebe gratis por 15 días
+        </v-btn>
+      </v-col>
+      <v-col cols="6">
+        <div>Ticket</div>
+      </v-col>
+    </v-row>
+  </content-section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
