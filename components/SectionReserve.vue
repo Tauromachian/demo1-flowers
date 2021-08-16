8b9f5743a27b4b8672d21b33de87348074891b37
Jose Garcia <jogarcia@tuta.io>
2021-08-16 16:20:58 -0400

    Change text are style

--- components/SectionReserve.vue
+++ components/SectionReserve.vue
@@ -1,8 +1,10 @@
 <template>
   <content-section title="Encuentra y reserva el servicio que necesitas">
     <v-form>
-      <v-text-field label="Qué" />
-      <v-text-field label="Dónde" />
+      <v-label>Qué</v-label>
+      <v-text-field solo label="Nombre del servicio o del proveedor" />
+      <v-label>Dónde</v-label>
+      <v-text-field solo label="Provincia, municipio, concejo popular" />
       <v-btn>Buscar</v-btn>
     </v-form>
   </content-section>
