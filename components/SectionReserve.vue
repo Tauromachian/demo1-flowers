0883533bd40b02a5b85bf1168783d01710673255
Jose Garcia <jogarcia@tuta.io>
2021-08-16 16:43:18 -0400

    Use v-container

--- components/SectionReserve.vue
+++ components/SectionReserve.vue
@@ -1,16 +1,20 @@
 <template>
-  <content-section title="Encuentra y reserva el servicio que necesitas">
-    <v-form>
-      <v-label>Qué</v-label>
-      <v-text-field solo label="Nombre del servicio o del proveedor" />
-      <v-label>Dónde</v-label>
-      <v-text-field solo label="Provincia, municipio, concejo popular" />
-      <div class="d-flex justify-center">
-        <v-btn>
-          Buscar
-        </v-btn>
-      </div>
-    </v-form>
+  <content-section title="Encuentra y reserva el servicio que necesitas" fluid>
+    <div class="blue">
+      <v-container>
+        <v-form>
+          <v-label>Qué</v-label>
+          <v-text-field solo label="Nombre del servicio o del proveedor" />
+          <v-label>Dónde</v-label>
+          <v-text-field solo label="Provincia, municipio, concejo popular" />
+          <div class="d-flex justify-center">
+            <v-btn>
+              Buscar
+            </v-btn>
+          </div>
+        </v-form>
+      </v-container>
+    </div>
   </content-section>
 </template>
 
