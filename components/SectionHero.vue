5ddac53ae09009b75bd6e182c76931b4a1bc02d3
Jose Garcia <jogarcia@tuta.io>
2021-08-16 16:17:57 -0400

    Create basic circles

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -20,7 +20,15 @@
         </v-btn>
       </v-col>
       <v-col cols="6">
-        <div>Ticket</div>
+        <div class="circle-outer">
+          <div class="circle-middle">
+            <div class="circle-inner">
+              <p>
+                Ticket
+              </p>
+            </div>
+          </div>
+        </div>
       </v-col>
     </v-row>
   </content-section>
@@ -37,5 +45,35 @@ export default {}
 }
 .text-uppercase {
   text-transform: uppercase;
+}
+
+.circle-outer, .circle-middle {
+  border-radius: 50%;
+  opacity: 1;
+  height: 100%;
+  width: auto;
+  border: 1px solid black;
+}
+
+.circle-outer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.circle-middle {
+  height: 80%;
+  padding: 3em;
+}
+
+.circle-inner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background-color: #185596;
+  text-align: center;
+  height: 100%;
+  width: 100%;
 }
 </style>
