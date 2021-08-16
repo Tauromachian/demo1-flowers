0301f28c61e7eeae4629f13ba543e69394788d28
Jose Garcia <jogarcia@tuta.io>
2021-08-16 16:45:26 -0400

    Add v-container

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,36 +1,38 @@
 <template>
   <content-section>
-    <v-row>
-      <v-col cols="6">
-        <h1 class="text-uppercase section-hero__title">
-          Ticket
-        </h1>
-        <p>
-          Es la forma más fácil y rápida de gestionar su agenda de servicios.
-          Convierta su nuevo cliente en un cliente habitual, ofreciendo atención
-          diferenciada y sin hacerlo esperar
-        </p>
-        <div class="d-flex justify-center">
-          <v-btn color="primary align-self-center">
-            Registre su negocio
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <h1 class="text-uppercase section-hero__title">
+            Ticket
+          </h1>
+          <p>
+            Es la forma más fácil y rápida de gestionar su agenda de servicios.
+            Convierta su nuevo cliente en un cliente habitual, ofreciendo atención
+            diferenciada y sin hacerlo esperar
+          </p>
+          <div class="d-flex justify-center">
+            <v-btn color="primary align-self-center">
+              Registre su negocio
+            </v-btn>
+          </div>
+          <v-btn text>
+            Y pruebe gratis por 15 días
           </v-btn>
-        </div>
-        <v-btn text>
-          Y pruebe gratis por 15 días
-        </v-btn>
-      </v-col>
-      <v-col cols="6">
-        <div class="circle-outer">
-          <div class="circle-middle">
-            <div class="circle-inner">
-              <p>
-                Ticket
-              </p>
+        </v-col>
+        <v-col cols="6">
+          <div class="circle-outer">
+            <div class="circle-middle">
+              <div class="circle-inner">
+                <p>
+                  Ticket
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </v-col>
-    </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
   </content-section>
 </template>
 
