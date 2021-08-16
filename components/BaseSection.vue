f4a7f7808774de4a3a14d55b84eeab0cb42c9b19
Jose Garcia <jogarcia@tuta.io>
2021-08-16 16:38:55 -0400

    Add fluid mode

--- components/BaseSection.vue
+++ components/BaseSection.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="my-10">
+  <div class="my-10" :class="{ 'width-100': fluid }">
     <slot />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BaseSection'
+  name: 'BaseSection',
+  props: {
+    fluid: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
 <style>
-
+.width-100 {
+  width: 100%;
+}
 </style>
