3b835c201cb62cae4da0583d52ffb873ed2f422f
Jose Garcia <jogarcia@tuta.io>
2021-08-17 19:41:46 -0400

    Add gradient

--- components/BaseFooter.vue
+++ components/BaseFooter.vue
@@ -8,44 +8,30 @@
   >
     <v-container class="container">
       <v-row>
-        <v-col
-          cols="3"
-          class="pr-3"
-        >
+        <v-col md="3" cols="12" class="pr-3">
           <h1>Ticket</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem odit quis voluptatem.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem odit
+            quis voluptatem.
           </p>
         </v-col>
 
-        <v-col
-          cols="3"
-          class="px-3"
-        >
+        <v-col md="3" cols="6" class="px-3">
           <h3 class="mb-5">
             Síguenos en
           </h3>
-          <v-btn icon>
+          <v-btn
+            v-for="(socialButton, index) in social"
+            :key="socialButton + index"
+            icon
+          >
             <v-icon>
-              mdi-twitter
-            </v-icon>
-          </v-btn>
-          <v-btn icon>
-            <v-icon>
-              mdi-instagram
-            </v-icon>
-          </v-btn>
-          <v-btn icon>
-            <v-icon>
-              mdi-facebook
+              {{ "mdi-" + socialButton }}
             </v-icon>
           </v-btn>
         </v-col>
 
-        <v-col
-          cols="3"
-          class="px-3"
-        >
+        <v-col md="3" cols="6" class="px-3">
           <h3 class="mb-5">
             Contactos
           </h3>
@@ -53,12 +39,9 @@
           <p>Teléfono</p>
         </v-col>
 
-        <v-col
-          cols="3"
-          class="d-flex flex-column justify-center pl-3"
-        >
+        <v-col md="3" cols="12" class="d-flex flex-column justify-center pl-3">
           <v-btn class="mb-2 last-update-btn">
-            Fecha: 8-7-2021
+            Feach: 8-7-2021
           </v-btn>
           <div />
           <v-btn class="mb-2 last-update-btn">
@@ -70,19 +53,11 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-container
-      fluid
-      class="footer__copyright py-6 container"
-    >
-      <v-row
-
-        align-content="center"
-      >
-        <v-col
-          class="text-center"
-          cols="12"
-        >
-          Copyright &copy; {{ (new Date()).getFullYear() }} Ticket Todos los derechos reservados
+    <v-container fluid class="footer__copyright py-6 container">
+      <v-row align-content="center">
+        <v-col class="text-center" cols="12">
+          Copyright &copy; {{ new Date().getFullYear() }} Ticket Todos los
+          derechos reservados
         </v-col>
       </v-row>
     </v-container>
@@ -101,12 +76,7 @@ export default {
   },
 
   data: () => ({
-    social: [
-      // 'Facebook',
-      // 'Twitter',
-      // 'Instagram',
-      // 'Linkedin',
-    ]
+    social: ['twitter', 'instagram', 'facebook']
   })
 }
 </script>
@@ -126,7 +96,7 @@ p {
 }
 
 .last-update-btn {
-  background-color:RGB(0, 191, 165, 0.4) !important;
+  background-color: RGB(0, 191, 165, 0.4) !important;
 }
 
 .container {
@@ -134,16 +104,21 @@ p {
 }
 
 .footer {
-  background-color: #185596 !important;
+  background: rgb(31, 103, 177);
+  background: linear-gradient(
+    90deg,
+    rgba(31, 103, 177, 1) 6%,
+    rgba(7, 46, 89, 1) 89%
+  );
   overflow: hidden;
 }
 
 .footer__copyright {
-  background-color: rgba(17, 81, 145, 0.7);
+  background-color: rgb(7, 46, 89, 0.6);
 }
 
 .footer::before {
-  content: '';
+  content: "";
   display: block;
   position: absolute;
   height: 40em;
@@ -154,5 +129,4 @@ p {
   bottom: -33em;
   border-top: 2.4em solid #10457c !important;
 }
-
 </style>
