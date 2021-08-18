cf47fd9b5dc21c6d9a59984173028fe1bd4cdf9f
Jose Garcia <jogarcia@tuta.io>
2021-08-18 12:14:52 -0400

    Set structure of card

--- components/CarouselSection.vue
+++ components/CarouselSection.vue
@@ -1,7 +1,30 @@
 <template>
   <content-section :title="title">
-    <v-carousel v-if="slides.length" multiple>
-      <v-carousel-item v-for="(slide, index) in slides" :key="index" />
+    <v-carousel v-if="slides.length">
+      <v-carousel-item
+        v-for="(slide, index) in slides"
+        :key="slide.img + index"
+      >
+        <v-card>
+          <v-img :src="slide.img" alt="" />
+          <v-card-text>
+            <v-card-title>
+              {{ slide.serviceName }}
+            </v-card-title>
+            <v-card-title>
+              {{ slide.establishmentName }}
+            </v-card-title>
+            <p>
+              {{ slide.description }}
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text>
+              Reservar ya
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-carousel-item>
     </v-carousel>
   </content-section>
 </template>
@@ -25,6 +48,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
