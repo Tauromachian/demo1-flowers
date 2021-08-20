af15c3c15dbc0b584af8f1be81317618cb889528
Jose Garcia <jogarcia@tuta.io>
2021-08-19 20:36:20 -0400

    Use vueper slides instead of v-carousel

--- components/CarouselSection.vue
+++ components/CarouselSection.vue
@@ -1,39 +1,54 @@
 <template>
   <content-section :title="title">
-    <v-carousel v-if="slides.length">
-      <v-carousel-item
+    <vueper-slides
+      v-if="slides.length"
+      class="w-full no-shadow"
+      arrows-outside
+      transition-speed="250"
+      fixed-height="28em"
+      bullets-outside
+      :breakpoints="breakpoints"
+      :gap="2"
+    >
+      <vueper-slide
         v-for="(slide, index) in slides"
         :key="slide.img + index"
       >
-        <v-card>
-          <v-img :src="slide.img" alt="" />
-          <v-card-text>
-            <v-card-title>
-              {{ slide.serviceName }}
-            </v-card-title>
-            <v-card-title>
-              {{ slide.establishmentName }}
-            </v-card-title>
-            <p>
-              {{ slide.description }}
-            </p>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn text>
-              Reservar ya
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-carousel-item>
-    </v-carousel>
+        <template #content>
+          <v-card>
+            <v-img :src="slide.img" alt="" />
+            <v-card-text>
+              <v-card-title>
+                {{ slide.serviceName }}
+              </v-card-title>
+              <v-card-title>
+                {{ slide.establishmentName }}
+              </v-card-title>
+              <p>
+                {{ slide.description }}
+              </p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text>
+                Reservar ya
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </vueper-slide>
+    </vueper-slides>
   </content-section>
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
 import ContentSection from './ContentSection.vue'
+
 export default {
   components: {
-    ContentSection
+    ContentSection,
+    VueperSlides,
+    VueperSlide
   },
   props: {
     slides: {
@@ -44,8 +59,37 @@ export default {
       type: String,
       required: true
     }
+  },
+  data () {
+    return {
+      breakpoints: {
+        4000: {
+          visibleSlides: 3,
+          slideMultiple: 3
+        },
+        1100: {
+          visibleSlides: 2,
+          slideMultiple: 2
+        },
+        690: {
+          visibleSlides: 2,
+          slideMultiple: 2,
+          arrows: false,
+          bulletsOutside: true
+        },
+        600: {
+          arrows: false,
+          bulletsOutside: true
+        }
+      }
+    }
   }
 }
 </script>
 
-<style></style>
+<style>
+.w-full {
+  width: 100% !important;
+}
+
+</style>
