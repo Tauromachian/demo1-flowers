6071bd01a98552b983869cc2758b9476acdf42d4
Jose Garcia <jogarcia@tuta.io>
2021-08-23 08:40:16 -0400

    Set padding bottom for the section-reserve

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-column">
     <section-hero />
-    <section-reserve />
+    <section-reserve class="pb-10" />
     <section-public-services />
     <section-restaurants-and-bars />
     <section-recreation-and-culture />
