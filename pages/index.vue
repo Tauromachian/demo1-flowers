ca1d795ba298df8cf77313b99b9a9211cb3c23b3
Jose Garcia <jogarcia@tuta.io>
2021-08-28 10:02:50 -0400

    Remove SectionPublicServices

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <div class="d-flex flex-column">
     <section-hero />
     <section-reserve class="py-15" />
-    <section-public-services class="pt-15" />
     <section-restaurants-and-bars class="pt-15" />
     <section-recreation-and-culture />
     <section-beauty />
