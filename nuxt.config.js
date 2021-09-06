a6c3088262be9d2b78499a946fa5e6557d498201
Jose Garcia <jogarcia@tuta.io>
2021-09-06 16:13:33 -0400

    Change title font to Pacifico

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,11 +19,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Patua+One&display=swap'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap'
+        href: 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Pacifico&display=swap'
       }
     ]
   },
